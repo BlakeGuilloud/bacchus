76271bfea61f0b6df1ef81ea92d99231635ffa47
Forrest Guilloud <fbguillo@gmail.com>
2016-12-04 10:54:47 -0500

    Cleanup

--- src/docs/Navigation.jsx
+++ src/docs/Navigation.jsx
@@ -6,9 +6,9 @@ export default class Navigation extends Component {
 
   render() {
     return (
-      <ul>
-        <li>bacchus.</li>
-        <li>Buttons</li>
+      <ul className="app__navigation">
+        <li><Link to="/">bacchus.</Link></li>
+        <li><Link to="/button">Button</Link></li>
       </ul>
     );
   }
